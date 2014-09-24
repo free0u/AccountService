a84a976a813cbfb299962979ee97071b8e51cb3d
Anton Evdokimov <free0u@gmail.com>
2014-09-24 18:42:48 +0400

    Added function declarations

--- src/AccountService.java
+++ src/AccountService.java
@@ -1,15 +1,15 @@
+import java.rmi.Remote;
 import java.sql.SQLException;
 
 /**
  * Created by free0u on 9/11/14.
  */
-public interface AccountService {
+public interface AccountService extends Remote {
     /**
      * Retrieves current balance or zero if addAmount() method was not called before for specified id
      *
      * @param id balance identifier
      */
-
     Long getAmount(Integer id) throws SQLException;
 
     /**
@@ -18,6 +18,31 @@ public interface AccountService {
      * @param id    balance identifier
      * @param value positive or negative value, which must be added to current balance
      */
-
     void addAmount(Integer id, Long value) throws SQLException;
+
+    /**
+     * Returns count of call of specified function
+     *
+     * @param id    function identifier
+     * @return count of calls
+     * @throws SQLException
+     */
+    public Integer getCountRequestsAll(int id) throws SQLException;
+
+    /**
+     * Returns count of call of specified function in the last 'sec' seconds
+     *
+     * @param id    function identifier
+     * @param sec   count of seconds
+     * @return count of calls
+     * @throws SQLException
+     */
+    public Integer getCountRequestsPerInterval(int id, int sec) throws SQLException;
+
+    /**
+     *  Clears log in database
+     *
+     * @throws SQLException
+     */
+    public void clearLogs() throws SQLException;
 }
